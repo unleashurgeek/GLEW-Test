92f517cfd84636f413925341baee0349ae5fd326
Kyle Bartz <kyle@unleashurgeek.com>
2015-09-13 17:17:50 -0700

    Initial Commit

---
+++ src/main.c
@@ -0,0 +1,11 @@
+// A simple program
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <GLFW\glfw3.h>
+int main (int argc, char *argv[])
+{
+
+  fprintf(stdout, "Hello, World!");
+  return 0;
+}
