4adc00bf8eddf2445cd9740969c782f2c9e935f1
Kyle Bartz <kyle@unleashurgeek.com>
2015-09-13 19:48:01 -0700

    Renamed main.c to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 // A simple program
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+#define GLEW_STATIC
+#include <GL\glew.h>
 #include <GLFW\glfw3.h>
 int main (int argc, char *argv[])
 {
