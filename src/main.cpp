49e335d333b6aba3b1947ebc20567390a63044ec
Kyle Bartz <kyle@unleashurgeek.com>
2015-09-13 20:12:45 -0700

    Added basic GLFW window and GLEW initilization

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,46 @@
-// A simple program
-#include <stdio.h>
+#include <iostream>
+
 #define GLEW_STATIC
 #include <GL\glew.h>
 #include <GLFW\glfw3.h>
-int main (int argc, char *argv[])
-{
 
-  fprintf(stdout, "Hello, World!");
-  return 0;
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Test", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "Failed to initialize GLEW" << std::endl;
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+	std::cout << "Window Launched!" << std::endl;
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+	return 0;
 }
